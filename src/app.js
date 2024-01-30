99f6faa9d644e2a9304827cd9d6f4d024e2fc0b8
Maria Carrilho <a2019148211@isec.pt>
2024-01-30 16:09:55 +0000

    Logic and Modal

--- src/app.js
+++ src/app.js
@@ -3,24 +3,31 @@
 const nDifCardsEasy = 4;
 const nDifCardsNormal = 5;
 const nDifCardsHard = 9;
-let nCards = 4;
-let nColumns = 0;
 const max = 9;
 const min = 1;
-let idImage = [];
 
 // Variables
+let nColumns = 0;
+let matches = 0;
+let idImage = [];
+let nCards = 4;
+let cardsToPlay = document.querySelectorAll(".card.hidden");
 const cardContainer = document.querySelector(".container");
 const level = document.querySelector(".level");
 const startButton = document.querySelector(".start");
 let flippedCards = [];
+
 // Event Listeners
 startButton.addEventListener("click", function () {
+  idImage = [];
+  idImage.length = 0;
   if (level.value === "0") {
     alert("Please select a level!");
     return;
   }
-  let numbersOfCards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let numbersOfCards = [1, 2, 3, 4, 5, 6, 7, 8, 9].sort(
+    (a, b) => 0.5 - Math.random()
+  );
   if (level.value !== "hard")
     numbersOfCards = arrayWithRandomNumbers(nCards, min, max);
   let index = 0;
@@ -36,9 +43,13 @@ startButton.addEventListener("click", function () {
     if ((i - 1) % 2 == 0) index++;
     allCards[shuffle[i]].addEventListener("click", flipCard);
   }
+
+  startButton.innerText = "Pause";
+  startButton.classList.toggle("start");
 });
+
+// Functions
 function flipCard(event) {
-  let cardsToPlay = document.querySelectorAll(".card.hidden");
   let card = event.target;
   if (card.classList.toString().includes("hidden")) {
     const cardId = card.getAttribute("id");
@@ -59,8 +70,13 @@ function flipCard(event) {
         setTimeout(function () {
           hideCard(card1);
           hideCard(card2);
-        }, 1500);
-        console.log("here");
+        }, 1000);
+      } else {
+        matches++;
+        if (matches === nCards)
+          setTimeout(function () {
+            endGame();
+          }, 1000);
       }
       flippedCards.length = 0;
       flippedCards = [];
@@ -68,7 +84,20 @@ function flipCard(event) {
   }
 }
 
-// Functions
+function endGame() {
+  startButton.innerText = "Start";
+  startButton.classList.toggle("start");
+  document.querySelectorAll(".card.shown").forEach((card) => {
+    hideCard(card);
+  });
+  matches = 0;
+  document
+    .querySelector(".fa.fa-window-close")
+    .addEventListener("click", function () {
+      document.querySelector(".modal").style.display = "none";
+    });
+  document.querySelector(".modal").style.display = "block";
+}
 
 function getSelectedOption() {
   if (level.value === "easy" || level.value === "0") {
@@ -105,7 +134,7 @@ function hideCard(card) {
   card.style.backgroundImage = "none";
   card.style.backgroundColor = "#99b2dd";
   card.innerHTML =
-    '<i class="fa-solid fa-question" style="font-size:1.5rem"></i>';
+    '<i class="fa-solid fa-question" style="font-size:1.5rem; transform: scaleX(-1)"></i>';
   return card;
 }
 
@@ -117,7 +146,7 @@ function addCards() {
     card.classList.add("hidden");
     cardContainer.append(card);
     card.innerHTML =
-      '<i class="fa-solid fa-question" style="font-size:1.5rem"></i>';
+      '<i class="fa-solid fa-question" style="font-size:1.5rem; transform: scaleX(-1)"></i>';
   }
 }
 
